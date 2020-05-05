557828b414e39f5135435087a18f54178f5dcc84
GJQ Holdings Mac1 <shenay_yilmaz@hotmail.com>
2020-05-05 15:13:20 +0100

    redux create action generator && create redux store

--- src/index.js
+++ src/index.js
@@ -1,13 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
+import { store } from "./redux/store";
 import App from "./App";
 import "./index.css";
 
 import { BrowserRouter } from "react-router-dom";
 
 ReactDOM.render(
-  <Provider>
+  <Provider store={store}>
     <BrowserRouter>
       <App />
     </BrowserRouter>
